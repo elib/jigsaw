095c62a5e925608675190508f3c0c0196d2d90cc
elib <ewokker@gmail.com>
2012-08-07 19:42:39 +0300

    Improved the canvas/frame image, trying to set up offset so it looks good.

--- Jigsaw/Jigsaw/Jigsaw/Canvas.cs
+++ Jigsaw/Jigsaw/Jigsaw/Canvas.cs
@@ -11,22 +11,26 @@ namespace Jigsaw
     {
         public override Texture2D SetTexture(Microsoft.Xna.Framework.Content.ContentManager content)
         {
-            return content.Load<Texture2D>("canvas");
+            return content.Load<Texture2D>("canvas_frame");
         }
+
+        private const float _percentage = 8.0f / 64.0f;
 
         internal void setSize(int width, int height)
         {
-            //set offset according to border size
-            offSet = Vector2.Zero;
+            _size.X = width * (1.0f + _percentage * 2);
+            _size.Y = height * (1.0f + _percentage * 2);
 
-            _size.X = width;
-            _size.Y = height;
+            offSet = new Vector2(_percentage * width, _percentage * height);
 
             //center in field
-            _position.X = (float) Math.Floor((double)((Core.game.Width - width) / 2));
-            _position.Y = (float) Math.Floor((double)((Core.game.Height - height) / 2));
+            _position.X = (float)Math.Floor((double)((Core.game.Width - _size.X) / 2));
+            _position.Y = (float)Math.Floor((double)((Core.game.Height - _size.Y) / 2));
         }
 
-        public Vector2 offSet { get; private set; }
+        public Vector2 offSet
+        {
+            get; private set;
+        }
     }
 }
